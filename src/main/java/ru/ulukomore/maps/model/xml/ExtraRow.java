bd25e57cb7d7ffcb0763e223376ee737b3657cbc
d.i.ie <d.i.ie@tinkoff.ru>
2020-07-22 15:47:33 +0300

    fix + extra field

--- src/main/java/ru/ulukomore/maps/model/xml/ExtraRow.java
+++ src/main/java/ru/ulukomore/maps/model/xml/ExtraRow.java
@@ -4,14 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-import java.util.Optional;
 
 @Getter
 @Setter
